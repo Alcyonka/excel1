9e90aabbe25883872f8b6ddce5e122bdedff9962
A M <Alcyonespace@gmail.com>
2024-05-22 23:53:04 +0300

    123

---
+++ plateexcel1/src/App.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import "@fortune-sheet/react/dist/index.css"
+import {
+  useState,
+  useCallback,
+  useEffect,
+  useRef,
+  useMemo,
+} from "react";
+import { Workbook, WorkbookInstance } from "@fortune-sheet/react";
+import { Sheet, Op, Selection, colors } from "@fortune-sheet/core";
+import { v4 as uuidv4 } from "uuid";
+import { hashCode } from './utils';
+import SheetTst from './sheetTst';
+import {Example} from './Collabration.stories'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom'
+ 
+function App() {
+  return (
+    <div>
+      <Router>
+      <Routes>
+            <Route path="/" element={<Navigate to={`/documents/${uuidv4()}`} />} />
+            
+            <Route path="/documents/:id" element ={SheetTst} />
+  
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+ 
+export default App;
---
+++ plateexcel1/src/sheetTst.tsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import "@fortune-sheet/react/dist/index.css"
+import {
+  useState,
+  useCallback,
+  useEffect,
+  useRef,
+  useMemo,
+} from "react";
+import { Workbook, WorkbookInstance } from "@fortune-sheet/react";
+import { Sheet, Op, Selection, colors } from "@fortune-sheet/core";
+import { v4 as uuidv4 } from "uuid";
+import { hashCode } from './utils';
+ 
+const SheetTst = (): React.ReactNode => {
+    const [data, setData] = useState<Sheet[]>();
+    const wsRef = useRef<WebSocket>();
+    const workbookRef = useRef<WorkbookInstance>(null);
+    const lastSelection = useRef<any>();
+    const { username, userId } = useMemo(() => {
+        const _userId = uuidv4();
+        return { username: `User-${_userId.slice(0, 3)}`, userId: _userId };
+      }, []);
+      
+useEffect(() => {
+  const socket = new WebSocket("ws://localhost:8081/ws");
+  wsRef.current = socket;
+ 
+  socket.onopen = () => {
+    socket.send(JSON.stringify({ req: "getData" }));
+  };
+  // In useEffect
+socket.onmessage = (e: any) => {
+  const msg = JSON.parse(e.data);
+  if (msg.req === "getData") {
+    setData(msg.data);
+  } else if (msg.req === "op") {
+    workbookRef.current?.applyOp(msg.data);
+  }
+};
+ 
+}, []);
+ 
+  const onOp = useCallback((op: Op[]) => {
+    const socket = wsRef.current;
+    if (!socket) return;
+    socket.send(JSON.stringify({ req: "op", data: op }));
+  }, []);
+  
+  const onChange = useCallback((d: Sheet[]) => {
+    setData(d);
+  }, []);
+
+  const afterSelectionChange = useCallback(
+    (sheetId: string, selection: Selection) => {
+      const socket = wsRef.current;
+      if (!socket) return;
+      const s = {
+        r: selection.row[0],
+        c: selection.column[0],
+      };
+      if (
+        lastSelection.current?.r === s.r &&
+        lastSelection.current?.c === s.c
+      ) {
+        return;
+      }
+      lastSelection.current = s;
+      socket.send(
+        JSON.stringify({
+          req: "addPresences",
+          data: [
+            {
+              sheetId,
+              username,
+              userId,
+              color: colors[Math.abs(hashCode(userId)) % colors.length],
+              selection: s,
+            },
+          ],
+        })
+      );
+    },
+    [userId, username]
+  );
+ 
+// Workbook declaration
+if (!data){
+    return <div />;
+}
+else{
+  return (
+    <div style={{ width: "100%", height: "100vh" }}>
+        <Workbook ref={workbookRef} data={[{ name: "Sheet1" }]} onChange={onChange} onOp={onOp} hooks={{
+            afterSelectionChange,
+          }} />
+    </div>
+  );
+  }
+
+ 
+}
+ 
+export default SheetTst;
